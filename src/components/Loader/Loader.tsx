62dfae3e8ab28df772394a523cd0d70c1b34fd9b
Ihor <kotkote2409@gmail.com>
2024-06-25 13:03:37 +0300

    Update Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,15 +2,13 @@ import { Audio } from "react-loader-spinner";
 import css from "./Loader.module.css";
 export default function Loader() {
   return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="three-dots-loading"
-      wrapperStyle
-      wrapperClass
-      classname={css.loader}
-    />
+    <div className={css.loader}>
+      <Audio
+        height="80"
+        width="80"
+        color="green"
+        ariaLabel="three-dots-loading"
+      />
+    </div>
   );
 }
